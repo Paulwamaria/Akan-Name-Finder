30096ce184b199f910ab0697c744d8df3254c90d
Paulwamaria <paulwamaria@gmail.com>
2019-07-14 10:10:43 +0300

    Reorganise the functions and get rid of unused functions

--- js/akan.js
+++ js/akan.js
@@ -1,8 +1,19 @@
-//Create a function to collect data from the user
-var cC=19;
-var yY=90;
-var mM=6;
-var dD=30; 
+var dB, cC, yY, mM;
+//redeclare variables
+var dD=document.getElementById("date").value;
+var mM=document.getElementById("month").value;
+var yearOfBirth=document.getElementById("yearOfBirth").value;
+var cC = yearOfBirth.slice(0,2);
+var yY = yearOfBirth.slice(2,4);
+var dayName=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];  
+var genders = document.getElementById("gender");
+var gender = genders.options[genders.selectedIndex].text;
+
+
+
+
 //collect data from the form and output it
 function check(){
   document.getElementById("fd").innerHTML= "Here are your details: "
@@ -12,53 +23,42 @@ function check(){
   document.getElementById("f4").innerHTML= "Your gender is " + document.getElementById("gender").options[document.getElementById("gender").selectedIndex].text;
   
 }
-/*document.getElementById('theForm').onsubmit = function findAkanName(){
-  var dD = document.getElementById('date').value;
-  console.log(document.getElementById('theform').value);
-  var month = document.getElementById('month').value;
-  var yearOfBirth = document.getElementById('yearOfBirth').value;
-   var cC = yearOfBirth.slice(0,2);
-    var yY = yearOfBirth.slice(2,4);
-    var genders = document.getElementById("gender");
-  var gender = genders.options[genders.selectedIndex].text;
-  console.log("I love githeri");
-  
-  //giveName(gender);  
-  }*/
-//cC=parseInt((year.toString()).slice(0,2));
-//yY=parseInt((year.toString()).slice(2,4));
-//console.log(yY);
-function getDayWeek(cC,yY,mM,dD){
-  var dayOfWeek= ( ( (cC/4) -2*cC-1) + ((5*yY/4) ) + ((26*(mM+1)/10)) + dD ) % 7;
-  var result=Math.floor(dayOfWeek);
-  return result;
+//A function to find akan name
+
+//function getDayWeek
+function getDayWeek(cC,yY,mM,dD){var dD=document.getElementById("date").value;
+var mM=document.getElementById("month").value;
+var yearOfBirth=document.getElementById("yearOfBirth").value;
+var cC = yearOfBirth.slice(0,2);
+var yY = yearOfBirth.slice(2,4);
+
+  var dayNumber= parseInt(( ( (cC/4) -2*cC-1) + ((5*yY/4) ) + ((26*(mM+1)/10)) + dD ) % 7);
+  return dayNumber;
+ // document.getElementById("f6").innerHTML= "You were born on " + dayNumber;
   }
-  
-//console.log(getDayWeek(cC,yY,mM,dD));
-function convertD(){
-var dayName=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var i;
-for(i=0;i<dayName.length;i++){
-if(getDayWeek(cC,yY,mM,dD)===i){
-document.write(dayName[i]);
-}
-}
-}
+  getDayWeek(cC,yY,mM,dD);
 
-//A function to match day with Akan name
-male=true
-var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-var femNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-function giveName(gender){
-var j;
-for(j=0;j<maleNames.length; j++){
-if((getDayWeek(cC,yY,mM,dD)===j)&&(male===true)){
-window.alert(maleNames[j]);
-}else if((getDayWeek(cC,yY,mM,dD)===j)&&(gender===female)){
-window.alert(femNames[j]);
+  //function convert daynumber to actual day
+  function convertD(){
+    
+    var i;
+    for(i=0;i<dayName.length;i++){
+    if(getDayWeek(cC,yY,mM,dD)===i){
+     // document.getElementById("f6").innerHTML= "You were born on " + dayName[ getDayWeek(cC,yY,mM,dD)];
+    }
+    }
+    }
+    convertD();
 
-}
-}
-}
+function yourAkanName(){
+  var genders = document.getElementById("gender");
+  var gender = genders.options[genders.selectedIndex].text;
 
-
+  if(gender==="Male"){
+    document.getElementById("f6").innerHTML= "You were born on " + dayName[ getDayWeek(cC,yY,mM,dD)];
+    document.getElementById("f7").innerHTML= "Your Akan Name is " + maleNames[getDayWeek(cC,yY,mM,dD)];
+  }else if (gender==="Female"){
+    document.getElementById("f6").innerHTML= "You were born on " + dayName[ getDayWeek(cC,yY,mM,dD)];
+    document.getElementById("f7").innerHTML= "Your Akan Name is " + femaleNames[getDayWeek(cC,yY,mM,dD)];
+  }
+}
